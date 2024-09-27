f114534c6a879b239a6c8568083ddb52b576d817
Iker Galardi <contacto.ikergalardi@gmail.com>
2024-09-27 08:50:45 +0200

    Plat: create window and receive events

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
 CFLAGS=-O2 -march=native
+LDFLAGS=-lglfw
 
 OBJ_FILES=bin/main.o
 
 all: SuperAlienBro
 
 SuperAlienBro: $(OBJ_FILES)
-	gcc -o SuperAlienBro $(OBJ_FILES) $(CFLAGS)
+	gcc -o SuperAlienBro $(OBJ_FILES) $(CFLAGS) $(LDFLAGS)
 
 bin/%.o: src/%.c
 	gcc -c -o $@ $< $(CFLAGS)
--- src/main.c
+++ src/main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
+#include <GLFW/glfw3.h>
+
 int main(int argc, char** argv)
 {
-    printf("Hello world\n");
+    if (!glfwInit()) {
+        fprintf(stderr, "SuperAlienBro: an error occurred while initializing GLFW\n");
+
+        glfwTerminate();
+        return 1;
+    }
+
+    GLFWwindow *mainWindow = glfwCreateWindow(1000, 1000, "Super Alien Bro", NULL, NULL);
+    if (mainWindow == NULL) {
+        fprintf(stderr, "Could not create an OpenGL window\n");
+
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(mainWindow);
+
+    while (!glfwWindowShouldClose(mainWindow)) {
+        glfwSwapBuffers(mainWindow);
+        glfwPollEvents();
+    }
 }
