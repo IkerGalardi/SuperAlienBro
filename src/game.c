a94ad841f6a1bfdd8fd0aa4d28ded79f6612fd9a
Iker Galardi <contacto.ikergalardi@gmail.com>
2024-10-01 09:58:38 +0200

    Gfx: enable blending for transparent objects

--- src/game.c
+++ src/game.c
@@ -16,6 +16,9 @@ tilemap character_tilemap;
 
 void game_begin()
 {
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
     glClearColor(0.0f, 0.0f, 1.0f, 1.0);
 
     character_tilemap = tilemap_create("res/tilemaps/tilemap-characters_packed.png", 9, 3);
