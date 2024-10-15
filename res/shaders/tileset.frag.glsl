f83216f69397c2e566207c05f1f155f7ea0c8c56
Iker Galardi <contacto.ikergalardi@gmail.com>
2024-10-15 09:53:33 +0200

    Gfx: cleanup of shader code

--- res/shaders/tileset.frag.glsl
+++ res/shaders/tileset.frag.glsl
@@ -7,8 +7,7 @@ in Geometry
 
 out vec4 o_fragment;
 
-
-layout(binding = 0) uniform sampler2D u_character_tileset;
+layout(binding = 0) uniform sampler2D u_tileset;
 uniform bool u_horizontal_flip;
 uniform vec2 u_tileset_size;
 uniform vec2 u_tileset_render;
@@ -21,5 +20,5 @@ void main()
     }
 
     vec2 tileset_uv = uv / u_tileset_size + u_tileset_render / u_tileset_size;
-    o_fragment = texture(u_character_tileset, tileset_uv);
+    o_fragment = texture(u_tileset, tileset_uv);
 } 
