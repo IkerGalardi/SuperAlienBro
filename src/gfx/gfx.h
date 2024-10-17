8976f2c7d53969f989886b2b1fc42197591e86e1
Iker Galardi <contacto.ikergalardi@gmail.com>
2024-10-17 09:25:27 +0200

    Doc: document the graphics system functions

--- src/gfx/gfx.h
+++ src/gfx/gfx.h
@@ -43,17 +43,61 @@ typedef struct
     size_t _present_variations[3];
 } gfx_background;
 
-
+/**
+ * @brief Creates all the necessary resources to render tiles from a tileset file. 
+ * 
+ * Its important to `gfx_delete_tileset` when the tileset is no longer in use in order to save up
+ * memory.
+ * 
+ * @param path path to the file containing the tileset image
+ * @param x_tile_count number of tiles in the X axis
+ * @param y_tile_count number of tiles in the Y axis
+ * @return The tileset object
+ */
 gfx_tileset gfx_create_tileset(const char *path, uint8_t x_tile_count, uint8_t y_tile_count);
 
+/**
+ * @brief Deletes all the necessary resources used to render tiles and frees up memory.
+ * 
+ * @param tileset tileset to be deleted
+ */
 void gfx_delete_tileset(gfx_tileset *tileset);
 
+/**
+ * @brief Renders a tile in the tileset.
+ * 
+ * @param tileset tileset to be rendered
+ * @param tile_x position of the tile in the X axis
+ * @param tile_y position of the tile in the Y axis
+ * @param mvp model view projection matrix used to transform the object
+ * @param h_flip true in order to flip in the x axis, false otherwise
+ */
 void gfx_render_tile(gfx_tileset *tileset, uint8_t tile_x, uint8_t tile_y, mat4 mvp, bool h_flip);
 
+/**
+ * @brief Renders the correct animation frame
+ *
+ * @param animation animation to be rendered
+ * @param mvp model view projection matrix used to transform the object
+ * @param h_flip true in order to flip the x axis, false otherwise
+ */
 void gfx_render_animation(gfx_animation *animation, mat4 mvp, bool h_flip);
 
+/**
+ * @brief Creates all the necessary resources to render the background 
+ * 
+ * @param tileset tileset to be used to render the background
+ * @param variations variations of the same background to be added randomly
+ * @param variation_count number of variations passed in `variations`
+ * @return background object to be used on `gfx_render_background`
+ */
 gfx_background gfx_create_background(gfx_tileset *tileset, size_t *variations, size_t variation_count);
 
+/**
+ * @brief Renders the background
+ * 
+ * @param background background to be rendered
+ */
 void gfx_render_background(gfx_background *background);
 
 
